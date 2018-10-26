ad78d1131bb51cf9d8cc77720468bf85c72304f8
soberanesmajo <soberanesmajo@gmail.com>
2018-10-25 19:58:06 -0500

    Updates

---
+++ precache-manifest.7e92e95946702fbb6d21c91966d856f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "db3000449ee3cfd6d8bb",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/runtime~main.db300044.js"
+  },
+  {
+    "revision": "fd876416690f8fb06bd8",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/main.fd876416.chunk.js"
+  },
+  {
+    "revision": "fcc820ce449f4334b8fa",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/js/1.fcc820ce.chunk.js"
+  },
+  {
+    "revision": "fd876416690f8fb06bd8",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/static/css/main.7d91a5fe.chunk.css"
+  },
+  {
+    "revision": "0e3e590dbfa37d3d4f5a5226eaa4d3ba",
+    "url": "/cdmx-2018-06-bc-core-am-burger-queen/index.html"
+  }
+];
